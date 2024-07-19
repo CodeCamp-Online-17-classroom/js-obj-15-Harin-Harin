2881d7d661f175497b3d85a20c5e713ad0fa01e2
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-19 21:15:40 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+const notebook = {
+  brand: 'ASUS',
+  model: 'Vivobook D413IA-EB303TS',
+  processor: 'AMD Ryzen 7 4700U 3.6GHz',
+  graphics: 'Integrated Graphics : AMD Radeon Graphics',
+  ram: '8GB DDR4 Onboard',
+  storage: '512GB PCIe NVMe M.2 SSD'
+};
+
+let notebook2 = {};
+
+for (let key in notebook) {
+  Object.assign(notebook2, notebook)
+}
+
+console.log('notebook original ',notebook);
+console.log('notebook copy  ',notebook2);
